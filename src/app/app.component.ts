4ab95339a8c2c11c08b941377970fe54fcb2311a
ljoliveira <ljoliveira@luxfacta.com>
2023-08-16 01:45:00 -0300

    ajustes de estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drogaria_dias';
+  year: number = new Date().getUTCFullYear();
+
+  constructor() {
+    window.addEventListener('scroll', (event) => {
+      const page = document.querySelector('html');
+      page?.classList.add('page--scroll');
+    });
+  }
 }
